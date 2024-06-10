f32022ce2145bbb67d9ec9dcd81b651f57870c4f
shaniba17 <shanibatm2002@gmail.com>
2024-06-10 12:01:54 +0530

    view added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import AddFriends from './components/AddFriends';
+import ViewFriends from './components/ViewFriends';
 
 function App() {
   return (
     <div>
       <AddFriends/>
+      <ViewFriends/>
     </div>
   );
 }
--- src/components/ViewFriends.jsx
+++ src/components/ViewFriends.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
 
-const 
- = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+const ViewFriends = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Friend Name</th>
+                                        <th scope="col">Friend Nick Name</th>
+                                        <th scope="col">Describe Your Friend</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default 
+export default ViewFriends
